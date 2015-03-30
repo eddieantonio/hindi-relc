9708535ce2c0f4d9aac5f5219c818ce21e0e7cf9
Eddie Antonio Santos <easantos@ualberta.ca>
2015-03-30 11:28:39 -0600

    Added worst templates ever.

---
+++ js/gloss-table.js
@@ -0,0 +1,12 @@
+(function () {
+    var template = document.getElementById('table-template').innerText;
+
+    function joinText(pair) {
+        return pair.gloss.join('');
+    }
+    
+    function toTable(glossList) {
+    }
+
+    return toTable;
+}());
